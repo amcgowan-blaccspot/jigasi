0588b4f6e12bec591ea8fe483717a58a03b4ed47
Andrew McGowan <andrewmcgowan@AndrewMcGownMacbookTouch.local>
2018-11-02 15:36:11 -0700

    Make sure logging is as expected

--- src/main/java/org/jitsi/jigasi/Main.java
+++ src/main/java/org/jitsi/jigasi/Main.java
@@ -29,33 +29,30 @@ import org.jitsi.service.neomedia.*;
 import org.jitsi.util.*;
 
 /**
- * FIXME: update description
- * SIP gateway for Jitsi Videobridge conferences. Requires one XMPP and one SIP
- * account to be configured in sip-communicator.properties file. JVB conference
- * must be held on the same server as XMPP account. Currently after start the
- * conference held in {@link JvbConference#callContext} MUC is joined.
- * SIP account is used to dial {@link SipGateway} once we join
- * the conference. Work in progress...
+ * FIXME: update description SIP gateway for Jitsi Videobridge conferences.
+ * Requires one XMPP and one SIP account to be configured in
+ * sip-communicator.properties file. JVB conference must be held on the same
+ * server as XMPP account. Currently after start the conference held in
+ * {@link JvbConference#callContext} MUC is joined. SIP account is used to dial
+ * {@link SipGateway} once we join the conference. Work in progress...
  *
  * @author Pawel Domas
  */
-public class Main
-{
+public class Main {
     /**
-     * The name of the command-line argument which specifies the XMPP domain
-     * to use.
+     * The name of the command-line argument which specifies the XMPP domain to use.
      */
     private static final String DOMAIN_ARG_NAME = "--domain";
 
     /**
-     * The name of the command-line argument which specifies the IP address or
-     * the name of the XMPP host to connect to.
+     * The name of the command-line argument which specifies the IP address or the
+     * name of the XMPP host to connect to.
      */
     private static final String HOST_ARG_NAME = "--host";
 
     /**
-     * The default value of the {@link #HOST_ARG_NAME} command-line argument if
-     * it is not explicitly provided.
+     * The default value of the {@link #HOST_ARG_NAME} command-line argument if it
+     * is not explicitly provided.
      */
     private static final String HOST_ARG_VALUE = "localhost";
 
@@ -67,8 +64,8 @@ public class Main
     private static final String MAX_PORT_ARG_NAME = "--max-port";
 
     /**
-     * The default value of the {@link #MAX_PORT_ARG_NAME} command-line argument
-     * if it is not explicitly provided.
+     * The default value of the {@link #MAX_PORT_ARG_NAME} command-line argument if
+     * it is not explicitly provided.
      */
     private static final int MAX_PORT_ARG_VALUE = 20000;
 
@@ -80,34 +77,33 @@ public class Main
     private static final String MIN_PORT_ARG_NAME = "--min-port";
 
     /**
-     * The default value of the {@link #MIN_PORT_ARG_NAME} command-line argument
-     * if
+     * The default value of the {@link #MIN_PORT_ARG_NAME} command-line argument if
      * it is not explicitly provided.
      */
     private static final int MIN_PORT_ARG_VALUE = 10000;
 
     /**
-     * The name of the command-line argument which specifies the port of the
-     * XMPP host to connect on.
+     * The name of the command-line argument which specifies the port of the XMPP
+     * host to connect on.
      */
     private static final String PORT_ARG_NAME = "--port";
 
     /**
-     * The default value of the {@link #PORT_ARG_NAME} command-line argument if
-     * it is not explicitly provided.
+     * The default value of the {@link #PORT_ARG_NAME} command-line argument if it
+     * is not explicitly provided.
      */
     private static final int PORT_ARG_VALUE = 5347;
 
     /**
-     * The name of the command-line argument which specifies the secret key for
-     * the sub-domain of the Jabber component implemented by this application
-     * with which it is to authenticate to the XMPP server to connect to.
+     * The name of the command-line argument which specifies the secret key for the
+     * sub-domain of the Jabber component implemented by this application with which
+     * it is to authenticate to the XMPP server to connect to.
      */
     private static final String SECRET_ARG_NAME = "--secret";
 
     /**
-     * The name of the command-line argument which specifies sub-domain name for
-     * the jigasi component.
+     * The name of the command-line argument which specifies sub-domain name for the
+     * jigasi component.
      */
     private static final String SUBDOMAIN_ARG_NAME = "--subdomain";
 
@@ -123,18 +119,16 @@ public class Main
     private static final String LOGDIR_ARG_NAME = "--logdir";
 
     /**
-     * The name of the property that stores the home dir for application log
-     * files (not history).
+     * The name of the property that stores the home dir for application log files
+     * (not history).
      */
-    public static final String PNAME_SC_LOG_DIR_LOCATION =
-        "net.java.sip.communicator.SC_LOG_DIR_LOCATION";
+    public static final String PNAME_SC_LOG_DIR_LOCATION = "net.java.sip.communicator.SC_LOG_DIR_LOCATION";
 
     /**
-     * The name of the property that stores the home dir for cache data, such
-     * as avatars and spelling dictionaries.
+     * The name of the property that stores the home dir for cache data, such as
+     * avatars and spelling dictionaries.
      */
-    public static final String PNAME_SC_CACHE_DIR_LOCATION =
-        "net.java.sip.communicator.SC_CACHE_DIR_LOCATION";
+    public static final String PNAME_SC_CACHE_DIR_LOCATION = "net.java.sip.communicator.SC_CACHE_DIR_LOCATION";
 
     /**
      * The name of the command-line argument which specifies config folder to use.
@@ -146,9 +140,18 @@ public class Main
      */
     private static final String CONFIG_DIR_NAME_ARG_NAME = "--configdirname";
 
+    private final static Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args)
         throws ParseException
     {
+        logger.setLevelAll();
+        logger.debug("[TESTLOG] Debug enabled");
+        logger.info("[TESTLOG] Info enabled");
+        logger.warn("[TESTLOG] Warn enabled");
+        logger.error("[TESTLOG] error enabled");
+        logger.fatal("[TESTLOG] Falta enabled");
+
         // Parse the command-line arguments.
         CmdLine cmdLine = new CmdLine();
 
